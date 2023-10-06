9f265b71f4d5d6c311ac2405242b9e58323cc840
nik7778 <107151173+nik7778@users.noreply.github.com>
2023-10-06 20:50:44 +0300

    Add files via upload

---
+++ InvoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using Invoicing.Data;
+using Invoicing.Data.Entities;
+
+namespace Invoicing.Infrastructure.Data.Repositories
+{
+    public class InvoiceRepository : BaseMongoDbRepository<Invoice>
+    {
+        private const string CollectionName = "Invoices";
+        public InvoiceRepository(MongoDbContext context) : base(context)
+        {
+        }
+        protected override IMongoCollection<Invoice> Collection => _context.MongoDatabase.GetCollection<Invoice>(CollectionName);
+    }
+}
